0a1f3ad075bfd04e40300cae52c6bbb50fe4b9ca
Jan Kuhfeld <jan@kuhfeld.net>
2025-04-18 21:45:47 +0900

    remove echo

--- conf.d/gpg_ssh.fish
+++ conf.d/gpg_ssh.fish
@@ -1,7 +1,6 @@
 set -e SSH_AGENT_PID
 set local_user (not set -q SSH_CLIENT)
 if set -q SSH_CLIENT
-  echo "SSH user" 
   else if not set -q gnupg_SSH_AUTH_SOCK_by or test $gnupg_SSH_AUTH_SOCK_by -ne $fish_pid
     set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket) 
 end
